d14fa65d5dde66e68e263cd6cc028fb64d077733
Emmanuel Herrera <emherrer1@gmail.com>
2023-06-18 01:42:08 -0400

    se agrgego codio a gitignore y metodo complete a todo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type: add, show, edit, complete or exit.")
+    user_action = input("Type: add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action == "add":
@@ -9,14 +9,18 @@ while True:
         todos.append(todo)
 
     elif user_action == "show":
-        for idx, item in todos:
+        for idx, item in enumerate(todos):
             print(f"{idx + 1} -- {item}")
 
     elif user_action == "edit":
-        pass
+        number = int(input("Choose the number of the todo to edit: "))
+        new_todo = input("Enter new todo: ")
+        todos[number -1]= new_todo
 
     elif user_action == "complete":
-        pass
+        number = int(input("Number of the todo completed: "))
+        todos.pop(number-1)
+
 
     elif user_action == "exit":
         break
